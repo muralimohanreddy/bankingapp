7a9c1dabc25ccb18044dad99ffb442a48518f37b
unknown <muralimohanredy@gmail.com>
2019-03-27 16:16:55 +0530

    banking projects

--- src/main/java/com/cgi/banking/bankingAPI/BankingApiApplication.java
+++ src/main/java/com/cgi/banking/bankingAPI/BankingApiApplication.java
@@ -3,11 +3,14 @@ package com.cgi.banking.bankingAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.cgi.banking.bankingApI")
 @EntityScan(basePackages ="com.cgi.banking.bankingAPI.*")
+@EnableDiscoveryClient
 public class BankingApiApplication {
 
 	public static void main(String[] args) {
